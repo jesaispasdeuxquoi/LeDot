f292ce430a67d9b47b4febf63f39ed6e2af9b675
LeoMastaa <frittofx@gmail.com>
2023-01-26 20:16:32 +0100

    removed useless dir

--- config/suckless/desktop/dmenu/config.h
+++ config/suckless/desktop/dmenu/config.h
@@ -1,37 +1,25 @@
 /* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
 
-static int topbar = 1;			/* -b option; if 0, dmenu appears at bottom */
-static int centered = 1;		/* -c option; centers dmenu on screen */
-static int min_width = 200;		/* minimum width when centered */
-static int fuzzy = 1;			/* -F option; if 0, dmenu doesn't use fuzzy matching */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 200;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"cascadia code:size=11.5:antialias=true:autohint=true",
+	"terminus:size=12"
 };
-
-static const char col_soft_black[] = "#111010";
-static const char col_gray[] = "#a89c90";
-static const char col_dirty_white[] = "#a89c90";
-static const char col_red[] = "#d75f5f";
-static const char col_dark_yellow[] = "#f7ca88";
-static const char col_white[] = "#111010";
-static const char col_green[] = "#a89c90";
-static const char col_blue[] = "#8197bf";
-static const char *prompt = NULL;	/* -p option; prompt to the left of input field */
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/* fg bg */
-	[SchemeNorm] = {col_dirty_white, col_soft_black},	// Normal
-	[SchemeSel] = {col_white, col_gray},	// Selected
-	[SchemeSelHighlight] = {col_green, col_soft_black},
-	[SchemeNormHighlight] = {col_green, col_gray},
-	[SchemeOut] = {col_dirty_white, col_soft_black},
-	[SchemeMid] = {col_dirty_white, col_soft_black},
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#101010" },
+	[SchemeSel] = { "#101010", "#805adb" },
+	[SchemeOut] = { "#ffffff", "#805adb" },
 };
-
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines = 0;
-static unsigned int lineheight = 22;	/* -h option; minimum height of a menu line */
-static unsigned int columns = 0;
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 10;
+static unsigned int min_lineheight = 10;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -40,4 +28,4 @@ static unsigned int columns = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;	/* -bw option; to add border width */
+static const unsigned int border_width = 2;
